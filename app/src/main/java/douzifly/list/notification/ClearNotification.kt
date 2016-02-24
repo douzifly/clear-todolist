4b4413321f82e913da518e0d92d7b0ea7fea8aaf
douzifly <douzifly@gmail.com>
2016-02-24 12:04:15 +0800

    into the corresponding group when click reminder notification

--- app/src/main/java/douzifly/list/notification/ClearNotification.kt
+++ app/src/main/java/douzifly/list/notification/ClearNotification.kt
@@ -8,29 +8,34 @@ import android.content.Intent
 import android.support.v7.app.NotificationCompat
 import douzifly.list.R
 import douzifly.list.ui.home.MainActivity
+import douzifly.list.utils.logd
 
 /**
  * Created by air on 15/10/20.
  */
 object ClearNotification {
 
-  fun show(context: Context, title: String, color: Int, id: Int) {
-    val intent = Intent(Intent.ACTION_MAIN);
-    intent.addCategory(Intent.CATEGORY_LAUNCHER);
-    intent.setClass(context, MainActivity::class.java);
-    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or android.content.Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-    val pi = PendingIntent.getActivity(context, 0, intent, 0)
-    val notification = NotificationCompat.Builder(context)
-            .setColor(color)
-            .setContentText(title)
-            .setContentTitle(context.resources.getString(R.string.reminder))
-            .setSmallIcon(R.drawable.ic_small_icon)
-            .setAutoCancel(true)
-            .setContentIntent(pi)
-            .setDefaults(Notification.DEFAULT_ALL)
-            .build()
-    val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    nm.notify(id, notification)
-  }
+    val EXTRA_THING_ID = "thingId"
+
+    fun show(context: Context, title: String, color: Int, id: Int) {
+        val intent = Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setClass(context, MainActivity::class.java);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or android.content.Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        intent.putExtra(EXTRA_THING_ID, id)
+        "pending intent thingid: $id".logd("MainActivity")
+        val pi = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        val notification = NotificationCompat.Builder(context)
+                .setColor(color)
+                .setContentText(title)
+                .setContentTitle(context.resources.getString(R.string.reminder))
+                .setSmallIcon(R.drawable.ic_small_icon)
+                .setAutoCancel(true)
+                .setContentIntent(pi)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .build()
+        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nm.notify(id, notification)
+    }
 
 }
